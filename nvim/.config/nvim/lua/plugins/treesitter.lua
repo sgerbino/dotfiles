e4651b43afaa2ab95e208cb86c8e54073dd4369f
Steve Gerbino <steve@gerbino.co>
2026-02-18 23:07:25 +0100

    Fix runtime path for new treesitter

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -6,6 +6,13 @@ return {
   config = function()
     local parsers = { 'bash', 'c', 'cpp', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query' }
 
+    -- The main branch stores queries under runtime/ which lazy.nvim doesn't add to rtp
+    local ts_path = vim.fs.joinpath(vim.fn.stdpath('data'), 'lazy', 'nvim-treesitter', 'runtime')
+    if vim.uv.fs_stat(ts_path) then
+      vim.opt.rtp:prepend(ts_path)
+    end
+
+    require('nvim-treesitter').setup()
     require('nvim-treesitter').install(parsers)
 
     vim.api.nvim_create_autocmd('FileType', {
