c8b029c6bd790ceb31455940e362597752ce041e
Steve Gerbino <steve@gerbino.co>
2026-02-23 19:36:03 +0100

    Fix treesitter highlight queries not found on FreeBSD

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -6,6 +6,9 @@ return {
   config = function()
     local parsers = { 'bash', 'c', 'cpp', 'diff', 'html', 'javascript', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'regex', 'rust', 'typescript', 'zig' }
 
+    -- FreeBSD doesn't automatically resolve nested runtime/ in plugins
+    vim.opt.runtimepath:append(vim.fn.stdpath('data') .. '/lazy/nvim-treesitter/runtime')
+
     require('nvim-treesitter').setup()
     require('nvim-treesitter').install(parsers)
 
