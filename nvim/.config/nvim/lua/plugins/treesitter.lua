bd2ad7e927454891a68337856c9094154b1da191
Steve Gerbino <steve@gerbino.co>
2026-02-23 19:44:50 +0100

    Defer treesitter runtime path append to survive Lazy.nvim rtp reset

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -6,11 +6,13 @@ return {
   config = function()
     local parsers = { 'bash', 'c', 'cpp', 'diff', 'html', 'javascript', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'regex', 'rust', 'typescript', 'zig' }
 
-    -- FreeBSD doesn't automatically resolve nested runtime/ in plugins
-    vim.opt.runtimepath:append(vim.fn.stdpath('data') .. '/lazy/nvim-treesitter/runtime')
-
     require('nvim-treesitter').setup()
     require('nvim-treesitter').install(parsers)
+
+    -- Deferred so it persists after Lazy.nvim finalizes runtimepath
+    vim.schedule(function()
+      vim.opt.runtimepath:append(vim.fn.stdpath('data') .. '/lazy/nvim-treesitter/runtime')
+    end)
 
     vim.api.nvim_create_autocmd('FileType', {
       pattern = '*',
